b7cf69d5846d4fef49039ac82d0439eab878160d
jair1823 <cordero1823.cr@gmail.com>
2019-05-09 01:44:51 -0600

    Seccion informacion agregada.

--- src/app/members/informacion/informacion.page.ts
+++ src/app/members/informacion/informacion.page.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformacionPage implements OnInit {
 
+  informacion = [
+    {
+      nombre: 'Bomberos',
+      telefono: '2435-4837',
+      descripcion: 'Brindamos servicios de prevención y protección para salvaguardar la vida, los bienes y el medio ambiente.'
+    },
+    {
+      nombre: 'Policía',
+      telefono: '2435-4837',
+      descripcion: 'Seguridad ante todo,'
+    },
+    {
+      nombre: 'OIJ',
+      telefono: '2435-4837',
+      descripcion: 'Organismo de Invesigación Judicial'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
