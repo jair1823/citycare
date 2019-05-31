16ada7b0bc4485c13f765f678e79f702afd1a2b4
jair1823 <cordero1823.cr@gmail.com>
2019-05-31 14:40:39 -0600

    Se crearon las paginas para mis publicaciones

--- src/app/members/member-routing.module.ts
+++ src/app/members/member-routing.module.ts
@@ -5,7 +5,10 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: './dashboard/dashboard.module#DashboardPageModule'
   },
-  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' }
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'noticias', loadChildren: './mis-publicaciones/tabs/noticias/noticias.module#NoticiasPageModule' },
+  { path: 'seguridad', loadChildren: './mis-publicaciones/tabs/seguridad/seguridad.module#SeguridadPageModule' },
+  { path: 'servicios', loadChildren: './mis-publicaciones/tabs/servicios/servicios.module#ServiciosPageModule' }
 ];
 
 @NgModule({
